29534f527ffc9c5b0b2502edac00af2022ef2a5a
Mhora19 <oreoluwafanimo2003@gmail.com>
2022-09-09 15:43:07 -0700

    opeful

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ * Description: prints "\"programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+int mainn(void)
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
