d9e6ef84cc735faacf96a08ce5fe9ae3ed478cdd
Mhora19 <oreoluwafanimo2003@gmail.com>
2022-09-12 12:11:40 -0700

    hopefully

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point  
- *
+ * main - Entry point
  * Description: prints "\"programming is like building a multilingual puzzle"
- *
  * Return: 0
  */
 int main(void)
