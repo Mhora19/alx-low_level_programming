6ff3464cb1344434d6084879861a06f90affe8e8
Mhora19 <oreoluwafanimo2003@gmail.com>
2022-09-15 16:18:14 -0700

    please work

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * prints_numbers - entry point
+ *
+ * Description: prints the numbers
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
